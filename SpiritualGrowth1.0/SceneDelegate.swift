91a6addf732fe1cca981b314096971d283c83901
briancordeau <bcordeau@gmail.com>
2023-03-07 18:08:38 -0600

    removed one load activity

--- SpiritualGrowth1.0/SceneDelegate.swift
+++ SpiritualGrowth1.0/SceneDelegate.swift
@@ -32,7 +32,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Called when the scene has moved from an inactive state to an active state.
         // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
        // print("scenedidbeconeactive")
-        loadActivities()
+        //loadActivities()
     }
 
     func sceneWillResignActive(_ scene: UIScene) {
